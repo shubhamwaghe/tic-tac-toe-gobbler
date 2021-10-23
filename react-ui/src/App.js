e036955ccdc877be4deceec861db4ab422a2120c
shubhamwaghe <shubhamwaghe@gmail.com>
2021-10-23 15:03:39 +0530

    Initial Commit

---
+++ react-ui/src/App.js
@@ -0,0 +1,43 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import logo from './logo.svg';
+import Game from './components/Game';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const [url, setUrl] = useState('/api');
+
+  const fetchData = useCallback(() => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        setMessage(json.message);
+        setIsFetching(false);
+      }).catch(e => {
+        setMessage(`API call failed: ${e}`);
+        setIsFetching(false);
+      })
+  }, [url]);
+
+  useEffect(() => {
+    setIsFetching(true);
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Game />
+      </header>
+    </div>
+  );
+
+}
+
+export default App;
