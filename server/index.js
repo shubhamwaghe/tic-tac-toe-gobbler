8b0ff6e34a01b73ca870e42b329dc5bbd2f4d428
shubhamwaghe <shubhamwaghe@gmail.com>
2021-12-19 20:09:08 +0530

    Socket IO - POC

--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+const http = require('http');
+const socketIo = require("socket.io");
 
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
+
+const STATIC_CHANNELS = ['global_notifications', 'global_chat'];
+
+
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
@@ -22,6 +28,9 @@ if (!isDev && cluster.isMaster) {
 } else {
   const app = express();
 
+  const server = http.Server(app);
+  var io = socketIo(server);
+
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
@@ -31,12 +40,30 @@ if (!isDev && cluster.isMaster) {
     res.send('{"message":"Hello from the custom server!"}');
   });
 
+  /* Sockets Code */
+  //Whenever someone connects this gets executed
+  io.on('connection', function(socket) {
+    console.log('A user connected', socket.id);
+    socket.join('channel-1');
+
+
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+      console.log('A user disconnected');
+    });
+
+    socket.on('move-piece', function (data) {
+      console.log(data);
+      socket.broadcast.in('channel-1').emit('move-piece', data); 
+    });
+  });
+
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
   });
 
-  app.listen(PORT, function () {
+  server.listen(PORT, function () {
     console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
   });
 }
