b92b6a979122e04e3fb9407a2cdecc6e9ca4cac5
shubhamwaghe <shubhamwaghe@gmail.com>
2023-02-18 22:37:45 +0530

    About Page Link update

--- react-ui/src/components/section/About.js
+++ react-ui/src/components/section/About.js
@@ -7,7 +7,7 @@ export default class About extends Component {
                 <section>
                     <h1>About</h1>
                     <h4>Game Launch Date: 31st December 2021</h4>
-
+                    <h5>Github: <a href="https://github.com/shubhamwaghe/tic-tac-toe-gobbler"> https://github.com/shubhamwaghe/tic-tac-toe-gobbler </a></h5>
                 </section>
             </div>
         )
